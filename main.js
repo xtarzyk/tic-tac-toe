1840b975ace68bcfd23e5d166d85dbd33ecc01ad
xtarzyk <xtarzyk@tutanota.com>
2022-04-11 16:20:14 +0200

    added creation of shapes on game board, first steps of random creation logic

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import {gameContainer} from './board'
+import {createCircle, createCross} from './utils'
 
 const app = document.getElementById('app')
 app.appendChild(gameContainer)
@@ -10,36 +11,59 @@ const choosePlayerX = () => playerX.addEventListener('click', event => {
     if (event.target.classList.contains('player-container__player-x')) {
         event.target.parentNode.classList.add('chosen-player')
         playerO.classList.add('disabled')
+        userSelection = 'x'
         return
     }
     if (event.target.classList.contains('x-second-arm')) {
         event.target.parentNode.parentNode.classList.add('chosen-player')
         playerO.classList.add('disabled')
+        userSelection = 'x'
         return
     }
     event.target.classList.add('chosen-player')
     playerO.classList.add('disabled')
-    console.log('x')
-}, { once:true })
+    userSelection = 'x'
+}, {once: true})
 
 const choosePlayerO = () => playerO.addEventListener('click', event => {
     if (event.target.classList.contains('player-container__player-o')) {
         event.target.parentNode.classList.add('chosen-player')
         playerX.classList.add('disabled')
+        userSelection = 'o'
         return
     }
     event.target.classList.add('chosen-player')
     playerX.classList.add('disabled')
-    console.log('o')
-}, { once:true })
+    userSelection = 'o'
+}, {once: true})
 
 
 const boardCells = document.querySelectorAll('.game-board__cell')
-const cellPick = () => boardCells.forEach(cell => {
+const randomIndex = Math.floor(Math.random() * boardCells.length)
+const cellPick = () => boardCells.forEach((cell, index) => {
     cell.addEventListener('click', event => {
-        console.log(event.target)
+        if (userSelection === 'x') {
+            createCross(cell)
+            event.target.classList.add('disabled')
+            return
+        }
+        if (userSelection === 'o') {
+            createCircle(cell)
+            event.target.classList.add('disabled')
+            createRandom()
+            return
+        }
     })
 })
+
+function createRandom() {
+    boardCells.forEach((cell, index) => {
+        if (boardCells[randomIndex].hasChildNodes()) {
+            console.log('fire')
+            createCircle(cell)
+        }
+    })
+}
 
 choosePlayerX()
 choosePlayerO()
---
+++ utils.js
@@ -0,0 +1,20 @@
+const createCircle = (parent) => {
+    const circle = document.createElement('div')
+    circle.classList.add('player-container__player-o')
+    parent.appendChild(circle)
+}
+
+const createCross = (parent) => {
+    const cross = document.createElement('div')
+    const secondArm = document.createElement('div')
+    cross.classList.add('player-container__player-x')
+    secondArm.classList.add('x-second-arm')
+    cross.appendChild(secondArm)
+    parent.appendChild(cross)
+}
+
+const createRandom = (shape, index) => {
+    
+}
+
+export { createCircle, createCross }
