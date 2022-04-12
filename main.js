84687aafe35fa099ce8e2c1de520a04e4470a335
xtarzyk <xtarzyk@tutanota.com>
2022-04-12 12:32:10 +0200

    random turns of computer player are done

--- main.js
+++ main.js
@@ -1,77 +1,69 @@
-import {gameContainer} from './board'
-import {createCircle, createCross} from './utils'
+import { gameContainer } from './board'
+import { createCircle, createCross, createRandom, positionsMap } from './utils'
 
 const app = document.getElementById('app')
 app.appendChild(gameContainer)
 const playerX = document.querySelector('.x')
 const playerO = document.querySelector('.o')
-let userSelection = null
+const boardCells = document.querySelectorAll('.game-board__cell')
+let userPlayer = ''
+let computerPlayer = ''
 
 const choosePlayerX = () => playerX.addEventListener('click', event => {
     if (event.target.classList.contains('player-container__player-x')) {
         event.target.parentNode.classList.add('chosen-player')
         playerO.classList.add('disabled')
-        userSelection = 'x'
+        userPlayer = 'x'
+        computerPlayer = 'o'
         return
     }
     if (event.target.classList.contains('x-second-arm')) {
         event.target.parentNode.parentNode.classList.add('chosen-player')
         playerO.classList.add('disabled')
-        userSelection = 'x'
+        userPlayer = 'x'
+        computerPlayer = 'o'
         return
     }
     event.target.classList.add('chosen-player')
     playerO.classList.add('disabled')
-    userSelection = 'x'
-}, {once: true})
+    userPlayer = 'x'
+    computerPlayer = 'o'
+}, { once: true })
 
 const choosePlayerO = () => playerO.addEventListener('click', event => {
     if (event.target.classList.contains('player-container__player-o')) {
         event.target.parentNode.classList.add('chosen-player')
         playerX.classList.add('disabled')
-        userSelection = 'o'
+        userPlayer = 'o'
+        computerPlayer = 'x'
         return
     }
     event.target.classList.add('chosen-player')
     playerX.classList.add('disabled')
-    userSelection = 'o'
-}, {once: true})
+    userPlayer = 'o'
+    computerPlayer = 'x'
+}, { once: true })
 
-
-const boardCells = document.querySelectorAll('.game-board__cell')
-const cellPick = () => boardCells.forEach(cell => {
+const cellPick = () => boardCells.forEach((cell, index) => {
     cell.addEventListener('click', event => {
-        if (userSelection === 'x') {
+        if (userPlayer === 'x') {
             createCross(cell)
             event.target.classList.add('disabled')
-            createRandom(boardCells, createCircle)
+            positionsMap.splice(index, 1, userPlayer)
+            createRandom(boardCells, createCircle, positionsMap, computerPlayer)
+            console.log(positionsMap)
             return
         }
-        if (userSelection === 'o') {
+        if (userPlayer === 'o') {
             createCircle(cell)
             event.target.classList.add('disabled')
-            createRandom(boardCells, createCross)
+            positionsMap.splice(index, 1, userPlayer)
+            createRandom(boardCells, createCross, positionsMap, computerPlayer)
+            console.log(positionsMap)
             return
         }
     })
 })
-
-const checkBoard = (array) => {
-
-}
-
-const createRandom = (array, createShape) => {
-    const randomIndex = () => Math.floor(Math.random() * boardCells.length)
-    let newRandom = randomIndex()
-    array.forEach((cell, index) => {
-        if (index === newRandom && !array[newRandom].hasChildNodes()) {
-            createShape(cell)
-            cell.classList.add('disabled')
-            return
-        }
-
-    })
-}
 
 choosePlayerX()
 choosePlayerO()
--- utils.js
+++ utils.js
@@ -1,3 +1,13 @@
+const positionsMap = Array.from({ length:9 })
+positionsMap.fill('')
+console.log(positionsMap)
+
+const checkBoard = () => positionsMap
+    .map((field, index) => field ? null : index)
+    .filter(value => value != null)
+
+console.log(checkBoard())
+
 const createCircle = (parent) => {
     const circle = document.createElement('div')
     circle.classList.add('player-container__player-o')
@@ -13,4 +23,24 @@ const createCross = (parent) => {
     parent.appendChild(cross)
 }
 
-export { createCircle, createCross }
+const createRandom = (array, createShape, positionMap, computerPlayer) => {
+    const spareIndexes = checkBoard()
+    const randomIndex = spareIndexes[Math.floor(Math.random() * spareIndexes.length)]
+    console.log(randomIndex)
+    array.forEach((cell, index) => {
+        if (index === randomIndex /*&& !array[randomIndex].hasChildNodes()*/) {
+            createShape(cell)
+            cell.classList.add('disabled')
+            positionsMap.splice(index, 1, computerPlayer)
+            console.log(checkBoard())
+            return
+        }
+    })
+}
+
+export {
+    createCircle,
+    createCross,
+    createRandom,
+    positionsMap
+}
